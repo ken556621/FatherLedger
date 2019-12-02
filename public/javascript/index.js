42d828a7d49a1579f1d21d63b0c8ccd65f3a07b7
ken <ken556621@gmail.com>
2019-12-02 13:52:02 +0800

    Add display mode not compelete

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,18 @@
 const dateInputField = document.getElementById('dateInputField');
 const dateInput = document.getElementById('dateInput');
 const showDateField = document.getElementById('showDateField');
+const sortDate = document.getElementById('sortDate');
 
 function showDateInput(){
     dateInputField.classList.toggle('d-none')
 }
 
+
 dateInput.addEventListener('input', function(){
     showDateField.innerHTML = dateInput.value;
 })
+
+// sortDate.addEventListener('click', function(){
+//     sortByDate();
+// })
 
--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const Account = require('../models/account');
 const moment = require('moment');
 const { authenticated } = require('../config/auth')
+const display = {
+    date: false,
+    price: false
+};
 
 
 
@@ -14,6 +18,11 @@ router.get('/', authenticated, (req, res) => {
         const paginationData = list.slice(0,10);
         for(let i =1;i <= totalPages;i++){
             pages.push(i);
+        }
+        if(req.query.date === 'on'){
+            paginationData.sort((a, b) => a.date - b.date);
+            display.date = true
+            res.render('index', { paginationData, pages, today });
         }
         if(err) console.log(err)
         return res.render('index', { paginationData, pages, today });
@@ -32,6 +41,37 @@ router.get('/page/:id', authenticated, (req, res) => {
         }
         if(err) console.log(err)
         return res.render('index', { paginationData, pages });
+    })
+})
+
+//dispaly mode
+router.get('/display/date', (req, res) => {
+    Account.find({ userId: req.user._id }, (err, list) => {
+        const today = moment().format('YYYYMMDD');
+        const totalPages = Math.ceil(list.length / 10) || 1;
+        const pages = [];
+        const paginationData = list.slice(0,10);
+        for(let i =1;i <= totalPages;i++){
+            pages.push(i);
+        }
+        paginationData.sort((a, b) => a.date - b.date);
+        display.date = true
+        res.render('index', { paginationData, pages, today });
+    })
+})
+
+router.get('/display/price', (req, res) => {
+    Account.find({ userId: req.user._id }, (err, list) => {
+        const today = moment().format('YYYYMMDD');
+        const totalPages = Math.ceil(list.length / 10) || 1;
+        const pages = [];
+        const paginationData = list.slice(0,10);
+        for(let i =1;i <= totalPages;i++){
+            pages.push(i);
+        }
+        paginationData.sort((a, b) => a.price - b.price);
+        display.price = true
+        res.render('index', { paginationData, pages, today });
     })
 })
 
